68f4aa99f06a2e197da89238e07399e8c39610f7
mariyamango <de.msomkova@gmail.com>
2024-10-01 09:51:07 +0200

    Initial commit

---
+++ src/main/java/com/example/recapProject1October/RecapProject1OctoberApplication.java
@@ -0,0 +1,13 @@
+package com.example.recapProject1October;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapProject1OctoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapProject1OctoberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recapProject1October/RecapProject1OctoberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recapProject1October;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapProject1OctoberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
